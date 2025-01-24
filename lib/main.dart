824ad5c33bf9b49572cefc8d480988d22a8a311d
kayllainecruz8 <kayllainecruz8@gmail.com>
2025-01-24 00:29:58 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+   debugShowCheckedBanner: false,
+      title: 'Layout',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Programa Layout'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Layout Superior
+            Expanded(
+              child: Container(
+                alignment: Alignment.center, // Corrigido Alignmente -> Alignment
+                child: const Text('Layout Superior'),
+              ),
+            ),
+            // Layout Central
+            Expanded(
+              flex: 7,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Container(
+                      // Espaço vazio, sem conteúdo
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Meu Aplicativo
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20), // Corrigido BorderRadius.ciircular
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 3,
+                  ),
+                  boxShadow: const [
+                    BoxShadow( // Corrigido Boxshadow -> BoxShadow
+                      color: Colors.black45,
+                      blurRadius: 10,
+                      offset: Offset(5, 5),
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  'Meu Aplicativo',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            // Layout Inferior
+            Expanded(
+              flex: 2,
+              child: Container(
+                alignment: Alignment.center,
+                child: const Text('Layout Inferior'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
